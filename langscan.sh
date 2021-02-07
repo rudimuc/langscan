0996530c828b8c0add34095835b9a7705dcd1d08
Rudi Häusler <commerce@haeuslernet.de>
2021-02-07 12:06:06 +0100

    First version of scanner script

---
+++ langscan.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+if [ -z "$1" ]
+then
+    echo "You need to specify a language"
+    exit 128
+fi
+
+if [ -z "$2" ]
+then
+    echo "You need to specify an inputfolder"
+    exit 128
+fi
+
+if [ -z "$3" ]
+then
+    echo "You need to specify an outputfolder"
+    exit 128
+fi
+
+language=$1
+folder=$2
+outputfolder=$3
+
+shopt -s globstar
+for i in **/*.mkv; do
+    # Do not process symbolic links
+    if [[ -L "$i" ]]
+    then
+        continue
+    fi
+
+    output=$(mkvmerge -F json --identify "$i" | jq '.tracks[] | select(.type=="audio") | select(.properties.language=="'"$language"'")')
+    if [ ! -z "$output" ]
+    then
+
+        filename=$(basename -- "$i")
+        echo $filename
+        ln -rsf "$i" "$3/$filename"
+    fi
+done
+
+
